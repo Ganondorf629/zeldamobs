f6382fcf37426f38caee71f76e2216f77a9acf8e
Ganondorf629 <ganondorf629@gmail.com>
2016-12-10 13:22:10 +0100

    Update ZeldaMobs.java

--- src/main/java/mod/mcreator/ZeldaMobs.java
+++ src/main/java/mod/mcreator/ZeldaMobs.java
@@ -84,13 +84,13 @@ public class TestEnvironmentMod implements IFuelHandler, IWorldGenerator {
 		mcreator_0.preInit(event);
 
 		ResourceLocation sound0 = new ResourceLocation("TestEnvironmentMod", "entity.gibdo.death");
-		GameRegistry.register(new net.minecraft.util.SoundEvent(sound52).setRegistryName(sound52));
+		GameRegistry.register(new net.minecraft.util.SoundEvent(sound52).setRegistryName(sound0));
 
 		ResourceLocation sound1 = new ResourceLocation("TestEnvironmentMod", "entity.gibdo.hurt");
-		GameRegistry.register(new net.minecraft.util.SoundEvent(sound53).setRegistryName(sound53));
+		GameRegistry.register(new net.minecraft.util.SoundEvent(sound53).setRegistryName(sound1));
 
 		ResourceLocation sound2 = new ResourceLocation("TestEnvironmentMod", "entity.gibdo.idle");
-		GameRegistry.register(new net.minecraft.util.SoundEvent(sound54).setRegistryName(sound54));
+		GameRegistry.register(new net.minecraft.util.SoundEvent(sound54).setRegistryName(sound2));
 
 	}
 

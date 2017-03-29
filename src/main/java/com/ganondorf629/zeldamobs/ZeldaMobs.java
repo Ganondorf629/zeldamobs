677da3dfdb848b8ec81d7ec263410c576dd866cc
Ganondorf629 <ganondorf629@gmail.com>
2017-03-29 17:18:44 +0200

    Update ZeldaMobs.java

--- src/main/java/com/ganondorf629/zeldamobs/ZeldaMobs.java
+++ src/main/java/com/ganondorf629/zeldamobs/ZeldaMobs.java
@@ -29,7 +29,7 @@ import java.util.Random;
 public class ZeldaMobs implements IFuelHandler, IWorldGenerator {
 
 	public static final String MODID = "ZeldaMobs";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.2.6";
 
 	@SidedProxy(clientSide = "com.ganondorf629.zeldamobs.ClientProxyZeldaMobs", serverSide = "com.ganondorf629.zeldamobs.CommonProxyZeldaMobs")
 	public static CommonProxyZeldaMobs proxy;

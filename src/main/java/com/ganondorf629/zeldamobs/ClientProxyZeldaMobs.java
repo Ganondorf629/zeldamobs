23bcac7d8087c3a49822048991ea1a5fb55da1eb
Ganondorf629 <ganondorf629@gmail.com>
2017-08-02 13:35:05 +0200

    Update ClientProxyZeldaMobs.java

--- src/main/java/com/ganondorf629/zeldamobs/ClientProxyZeldaMobs.java
+++ src/main/java/com/ganondorf629/zeldamobs/ClientProxyZeldaMobs.java
@@ -5,15 +5,15 @@ import com.ganondorf629.zeldamobs.ZeldaMobs;
 public class ClientProxyZeldaMobs extends CommonProxyZeldaMobs {
 
 	@Override
-	public void registerRenderers(ZeldaMobs ins) {
-		ins.mcreator_0.registerRenderers();
-		ins.mcreator_1.registerRenderers();
-		ins.mcreator_2.registerRenderers();
-		ins.mcreator_3.registerRenderers();
-		ins.mcreator_4.registerRenderers();
-		ins.mcreator_5.registerRenderers();
-		ins.mcreator_6.registerRenderers();
-		ins.mcreator_7.registerRenderers();
+	public void registerRenderers(zeldamobs ins) {
+		ins.zeldamobs_0.registerRenderers();
+		ins.zeldamobs_1.registerRenderers();
+		ins.zeldamobs_2.registerRenderers();
+		ins.zeldamobs_3.registerRenderers();
+		ins.zeldamobs_4.registerRenderers();
+		ins.zeldamobs_5.registerRenderers();
+		ins.zeldamobs_6.registerRenderers();
+		ins.zeldamobs_7.registerRenderers();
 
 	}
 }
